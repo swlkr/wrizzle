42f66d881cfbb5651aefc836f476c13eaa4889e5
Sarah Schulte <git@sarah.engineer>
2023-08-20 01:07:17 -0700

    polish

--- config.sh
+++ config.sh
@@ -1,2 +1,6 @@
 PROJECT_NAME=wrizzle
 TAILWIND=on
+
+if [[ ! -f data/wordoftheday ]]; then
+    shuf -n1 static/words > data/wordoftheday
+fi
---
+++ pages/form.sh
@@ -0,0 +1,27 @@
+
+source config.sh
+
+RESET_BUTTON='<button hx-post="/reset" hx-target="#guesses" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Reset</button>'
+
+GUESS_FORM="
+<input type=\"text\" name=guess id=guess placeholder=\"guess here\" class=\"uppercase\" autocomplete=\"off\"></input>
+<button _=\"on htmx:afterRequest get #guess then put '' into its value\" type=\"submit\" hx-post=\"/guess\" hx-target=\"#guesses\" class=\"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded\">Guess</button>
+"
+
+LAST_GUESS=$(tail -n1 data/guess)
+GUESS_COUNT=$(wc -l data/guess | cut -d' ' -f1)
+WORD=$(cat data/wordoftheday)
+
+if [[ "$LAST_GUESS" == "$WORD" ]]; then
+  FORM="<p>You won in $GUESS_COUNT guesses! :D</p>$RESET_BUTTON"
+elif [[ "$GUESS_COUNT" -gt 5 ]]; then
+  FORM="<p>Game over :( the correct word was '$WORD'</p>$RESET_BUTTON"
+else
+  FORM="$GUESS_FORM"
+fi
+
+htmx_page << EOF
+  <form hx-swap-oob="true" id="guess-form">
+    ${FORM}
+  </form>
+EOF
