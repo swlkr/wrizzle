01fb149578a981b39a1cbe0111a33bc520d5e69c
Sarah Schulte <git@sarah.engineer>
2023-08-20 01:22:37 -0700

    its a realtime app now

--- pages/form.sh
+++ pages/form.sh
@@ -1,11 +1,11 @@
 
 source config.sh
 
-RESET_BUTTON='<button hx-post="/reset" hx-target="#guesses" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Reset</button>'
+RESET_BUTTON='<button hx-post="/reset" hx-swap="none" class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Reset</button>'
 
 GUESS_FORM="
 <input type=\"text\" name=guess id=guess placeholder=\"guess here\" class=\"uppercase\" autocomplete=\"off\"></input>
-<button _=\"on htmx:afterRequest get #guess then put '' into its value\" type=\"submit\" hx-post=\"/guess\" hx-target=\"#guesses\" class=\"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded\">Guess</button>
+<button _=\"on htmx:afterRequest get #guess then put '' into its value\" type=\"submit\" hx-post=\"/guess\" hx-swap=\"none\" class=\"bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded\">Guess</button>
 "
 
 LAST_GUESS=$(tail -n1 data/guess)
--- pages/reset.sh
+++ pages/reset.sh
@@ -1,12 +1,9 @@
 # headers
-if [[ "$REQUEST_METHOD" != "POST" ]]; then
-  # only allow POST to this endpoint
-  return $(status_code 405)
+if [[ "$REQUEST_METHOD" == "POST" ]]; then
+  rm data/guess
+  touch data/guess
+  shuf -n1 static/words > data/wordoftheday
+  event reset | publish game
 fi
-
-rm data/guess
-touch data/guess
-
-shuf -n1 static/words > data/wordoftheday
 
 header "HX-Redirect" "/"
